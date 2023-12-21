00d558eee9619ff749f16f4857eb31e3c86eba7f
Viktor Zhabskyi <viktordevopscourse@gmail.com>
2023-12-21 15:34:36 +0200

    init install and pre commit script

--- cli/main.go
+++ cli/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+const (
+	GitLeakBinPAth = "./bin/gitleaks"
+)
+
+func isGitLeaksEnabled() bool {
+	out, err := exec.Command("git", "config", "--bool", "hooks.gitleaks").Output()
+	if err != nil {
+		return false
+	}
+	return strings.Contains(string(out), "true")
+}
+
+func isGitLeaksInstalled() bool {
+	cmd := exec.Command(GitLeakBinPAth, "version")
+	err := cmd.Run()
+	return err == nil
+}
 
 func main() {
-	fmt.Println("Pre commit working")
+	if !isGitLeaksInstalled() {
+		fmt.Println("gitleaks not installed")
+		return
+	}
+	if !isGitLeaksEnabled() {
+		fmt.Println("gitleaks precommit disabled (enable with `git config hooks.gitleaks true`)")
+		return
+	}
+
+	cmd := exec.Command(GitLeakBinPAth, "protect", "-v", "--staged", "--config", "./bin/config.toml", "--no-banner")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
---
+++ install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+VERSION="v8.18.1"
+TMP_FOLDER=gitleaks_tmp
+FOLDER="gitleaks-8.18.1"
+ARCHIVE="$FOLDER.zip"
+REPO_URL="https://github.com//gitleaks/gitleaks/archive/refs/tags/$VERSION.zip"
+
+install_GitLeaks(){
+
+  curl -sSLJO "$REPO_URL"
+
+  unzip -q $ARCHIVE \
+  && cd $FOLDER && make build
+
+  if mkdir -p "../../bin"; then
+    if [ -f "./gitleaks" ]; then
+       mv "./gitleaks" "../../bin/"
+      echo "Success installed: gitleaks"
+    else
+      echo "Some error occurred while gitleaks installing"
+    fi
+  else
+    echo "Error create a bin folder"
+  fi
+
+  cd ../ || exit
+}
+
+install_PreCommit(){
+  git clone https://github.com/ViktorDevOpsCourse/preCommitGitLeaksInstaller.git
+  cd ./preCommitGitLeaksInstaller && go build -o pre-commit ./cli
+  if [ -f "../../.git/hooks/pre-commit" ]; then
+      echo "You already using pre-commit hook"
+      echo -n "Do you want overwrite it [yes|no]: "
+      read -r adjustment
+      if [[ "$adjustment" == "yes" ]]; then
+          move_PreCommit
+      else
+        cd ../ || exit
+      fi
+  else
+      move_PreCommit
+  fi
+
+}
+
+move_PreCommit(){
+  mv "./pre-commit" "../../.git/hooks/"
+  cd ../ || exit
+}
+
+cleanUp() {
+  cd ../ || exit
+  rm -rf $TMP_FOLDER
+}
+
+if mkdir -p $TMP_FOLDER; then
+  echo "tmp folder success created"
+  cd "./$TMP_FOLDER" || exit
+else
+  echo "Failed create $TMP_FOLDER folder"
+fi
+
+echo "install gitleaks"
+install_GitLeaks
+
+echo "install pre-commit"
+install_PreCommit
+
+git config hooks.gitleaks true
+
+echo "clean up"
+cleanUp
+
+
